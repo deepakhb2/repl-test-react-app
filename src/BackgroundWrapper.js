c366e1fa0ecfed662820cf00b9e0b5ffda152a4a
Deepak Hagadur Bheemaraju <deepakhb2@gmail.com>
2020-07-24 17:30:58 +0000

    Add backgroundwrapper and medium button components

--- src/BackgroundWrapper.js
+++ src/BackgroundWrapper.js
@@ -2,10 +2,7 @@ import React from 'react';
 import MediumButton from './MediumButton'
 
 class BackgroundWrapper extends React.Component {
-  const Button = styled.button`
-  /* Adapt the colors based on primary prop */
-  background: ${props => props.primary ? "palevioletred" : "white"};
-  `
+  
     render() {
         return (
           <div style={{color: this.props.backgroundColor}}>
